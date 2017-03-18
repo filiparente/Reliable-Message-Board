cf792d3ea9f1183b8426695c57814e548cfd524b
filiparente <filipaoliveirarente@gmail.com>
2017-03-18 00:35:39 +0000

    está funcional

--- rmb.c
+++ rmb.c
@@ -8,22 +8,20 @@
 int main(int argc, char ** argv)
 {
 	struct sockaddr_in sid;
-	struct in_addr siip;
+	struct in_addr *siip;
 	struct hostent *h;
-	int sipt = 59000;
+	int sipt = 59000; /*se o utilizador não puser nada, assume-se porto 59000 e IP adress do servidor tejo*/
 
-	h = gethostbyname("tejo.tecnico.ulisboa.pt");
+	if((h = gethostbyname("tejo.tecnico.ulisboa.pt"))==NULL) exit(-1);
 
-	if(!inet_aton(h->h_addr_list[0] , &siip)){
-		exit(-1);
-	}
-/*teste*/
+	siip=(struct in_addr*)h->h_addr_list[0];
+
 
 	if(argc==3) /*caso em que so dão o ip ou o porto*/
 		{
 			if(!strcmp(argv[1],"-i"))
 				{
-					if(!inet_aton(argv[2] , &siip))
+					if(!inet_aton(argv[2] , siip))
 					{
 						exit(-1);
 					}
@@ -34,15 +32,15 @@ int main(int argc, char ** argv)
 				}
 
 		}
-	else if(argc==5)
+	else if(argc==5) /*caso em que dão o IP e o porto*/
 		{
-			if(!inet_aton(argv[2] , &siip)){
+			if(!inet_aton(argv[2] , siip)){
 				exit(-1);
 			}
-			sipt=atoi(argv[4]);
+			sipt=atoi(argv[4]);		
 		}
 
-		sid.sin_addr=siip;
+		sid.sin_addr=*siip;
 		sid.sin_port=sipt;
 
 		printf("server IP: %s server port: %d \n", inet_ntoa(sid.sin_addr), sid.sin_port);
@@ -50,3 +48,4 @@ int main(int argc, char ** argv)
 	return(0);
 
 }
+
